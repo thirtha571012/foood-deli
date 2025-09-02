21bcf08e9bbb321ed3f80657787d3ec484c5d78d
Silvers Tirtha <tirtharajbannerjee@gmail.com>
2025-09-02 16:37:08 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css';
 import Header from '../../components/Header/Header.jsx';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.jsx';
-import Appdownload from '../../components/AppDownload/Appdownload.jsx';
+import Appdownload from '../../components/AppDownload/AppDownload.jsx';
 const Home = () => {
 
   const [category,setCategory] =useState("All");
